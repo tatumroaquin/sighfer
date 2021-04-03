814b764493b5de4c0ea4ba71a3fa7cb2e2353669
ormux <ormuseldesu@gmail.com>
2021-04-04 05:09:24 +1000

    add quotes list

---
+++ js/quotes.js
@@ -0,0 +1,14 @@
+let quotes = [
+   "As I gaze upon the world, I *sigh* my life away to the hidden messages.",
+   "Sometimes it takes a little bit of eccentricity to realize the true nature of the universe.",
+   "I was once a humble programmer, now I am still a programmer but less humble.",
+   "I made this website so that I may provide a means for normal people to interpret the hidden.",
+   "If you are looking for happiness, you won't find it here, but I can make you understand what it means to be human.",
+   "Maybe you're a failure, maybe you're sucessful, either way don't be delusional, make an effort to see things for what they truly are.",
+   "The art of invincibility is not found by relying on people, because people are unreliable... being independent, self-sufficient, and reliable gives you power like no other.",
+   "I share my thoughts in encrypted form so that those who have the skills to decode it will likely read them, and hopefully they sleep wiser than they were yesterday.",
+   "Through imitation we learn, we memorise, we practice all that we know so that we may acquire our own originality.",
+   "Studying calculus has given me an insight about the concept of infinity, and I realised that it is an exact blueprint of the mind.",
+   "By observing evolution, making artificial intelligence, and learning the language of the machines, I saw profound truths about the world that I didn't realise before...",
+   "The world as you know it is an illusion, all that you see, hear, smell, taste, and touch are the five pillars of this great prison you call reality."
+];
